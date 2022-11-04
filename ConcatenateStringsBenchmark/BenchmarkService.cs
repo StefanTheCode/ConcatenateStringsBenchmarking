dd0f5e1acce2ea2a2047702728721ba6355a4bf0
Steven Giesel <stgiesel35@gmail.com>
2022-11-04 15:06:49 +0100

    Use file-scoped namespaces

--- ConcatenateStringsBenchmark/BenchmarkService.cs
+++ ConcatenateStringsBenchmark/BenchmarkService.cs
@@ -1,130 +1,129 @@
 ﻿using System.Text;
 using BenchmarkDotNet.Attributes;
 
-namespace ConcatenateStringsBenchmark
+namespace ConcatenateStringsBenchmark;
+
+[MemoryDiagnoser]
+public class BenchmarkService
 {
-    [MemoryDiagnoser]
-    public class BenchmarkService
+    [Params(10, 100, 1000)]
+    public  int Length;
+
+    public  string testString = "Test";
+
+    public  string[] appendix;
+
+    public BenchmarkService()
     {
-        [Params(10, 100, 1000)]
-        public  int Length;
+    }
 
-        public  string testString = "Test";
+    [GlobalSetup]
+    public void Setup()
+    {
+        this.appendix = new string[this.Length];
 
-        public  string[] appendix;
-
-        public BenchmarkService()
+        for (var i = 0; i < this.Length; i++)
         {
+            this.appendix[i] = "Test";
+        }
+    }
+
+    [Benchmark]
+    public string UsingPlus()
+    {
+        testString = "Test";
+
+        for (int i = 0; i < Length; i++)
+        {
+            testString += appendix[i];
         }
 
-        [GlobalSetup]
-        public void Setup()
-        {
-            this.appendix = new string[this.Length];
+        return testString;
+    }
 
-            for (var i = 0; i < this.Length; i++)
-            {
-                this.appendix[i] = "Test";
-            }
+    [Benchmark]
+    public string UsingConcat()
+    {
+        testString = "Test";
+
+        for (int i = 0; i < Length; i++)
+        {
+            testString = string.Concat(testString, appendix[i]);
         }
 
-        [Benchmark]
-        public string UsingPlus()
+        return testString;
+    }
+
+    [Benchmark]
+    public string UsingConcatWithArray()
+    {
+        testString = "Test";
+
+        testString = string.Concat(appendix);
+
+        return testString;
+    }
+
+    [Benchmark]
+    public string UsingJoin()
+    {
+        testString = "Test";
+
+        testString += string.Join(string.Empty, appendix);
+
+        return testString;
+
+    }
+
+    [Benchmark]
+    public string UsingFormat()
+    {
+        testString = "Test";
+
+        for (int i = 0; i < Length; i++)
         {
-            testString = "Test";
-
-            for (int i = 0; i < Length; i++)
-            {
-                testString += appendix[i];
-            }
-
-            return testString;
+            testString = string.Format("{0}{1}", testString, appendix[i]);
         }
 
-        [Benchmark]
-        public string UsingConcat()
+        return testString;
+    }
+
+    [Benchmark]
+    public string UsingStringBuilder()
+    {
+        testString = "Test";
+        StringBuilder builder = new StringBuilder(testString);
+
+        for (int i = 0; i < Length; i++)
         {
-            testString = "Test";
-
-            for (int i = 0; i < Length; i++)
-            {
-                testString = string.Concat(testString, appendix[i]);
-            }
-
-            return testString;
+            builder.Append(appendix[i]);
         }
 
-        [Benchmark]
-        public string UsingConcatWithArray()
-        {
-            testString = "Test";
+        testString = builder.ToString();
 
-            testString = string.Concat(appendix);
+        return testString;
+    }
 
-            return testString;
-        }
+    [Benchmark]
+    public string UsingStringBuilderAppendJoin()
+    {
+        testString = "Test";
 
-        [Benchmark]
-        public string UsingJoin()
-        {
-            testString = "Test";
+        StringBuilder builder = new StringBuilder(testString);
 
-            testString += string.Join(string.Empty, appendix);
+        builder.AppendJoin(String.Empty, appendix);
 
-            return testString;
+        testString = builder.ToString();
 
-        }
+        return testString;
+    }
 
-        [Benchmark]
-        public string UsingFormat()
-        {
-            testString = "Test";
+    [Benchmark]
+    public string UsingLinqAggregate()
+    {
+        testString = "Test";
+        testString += appendix.Aggregate((partialPhrase, word) => $"{partialPhrase}{word}");
 
-            for (int i = 0; i < Length; i++)
-            {
-                testString = string.Format("{0}{1}", testString, appendix[i]);
-            }
-
-            return testString;
-        }
-
-        [Benchmark]
-        public string UsingStringBuilder()
-        {
-            testString = "Test";
-            StringBuilder builder = new StringBuilder(testString);
-
-            for (int i = 0; i < Length; i++)
-            {
-                builder.Append(appendix[i]);
-            }
-
-            testString = builder.ToString();
-
-            return testString;
-        }
-
-        [Benchmark]
-        public string UsingStringBuilderAppendJoin()
-        {
-            testString = "Test";
-
-            StringBuilder builder = new StringBuilder(testString);
-
-            builder.AppendJoin(String.Empty, appendix);
-
-            testString = builder.ToString();
-
-            return testString;
-        }
-
-        [Benchmark]
-        public string UsingLinqAggregate()
-        {
-            testString = "Test";
-            testString += appendix.Aggregate((partialPhrase, word) => $"{partialPhrase}{word}");
-
-            return testString;
-        }
+        return testString;
     }
 }
